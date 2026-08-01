38196d172630c0f4d12ef70192d66454d9b780bd
jaycontr3ras <joancontreras08@gmail.com>
2026-08-01 19:54:40 -0400

    converted book array CRUD to Sequelize database operations

---
+++ Backend/models/book.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Book = sequelize.define("Book", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+  author: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = Book;
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const sequelize = require("./config/database");
+const Book = require("./models/Book");
 app.use(express.json());
 const PORT = 3000;
+
 
 const books = [
   {
@@ -16,19 +19,79 @@ app.get("/", (req,res) => {
     res.send("Api to track books is running");
 });
 
+sequelize.authenticate()
+  .then(() => console.log("Database connected"))
+  .catch((error) => console.log("Database connection failed", error));
+
+
+
+sequelize.sync()
+  .then(() => console.log("Tables synced"))
+  .catch(error => console.log(error));
+
 app.listen(PORT, () => {
-console.log(`Server Is Runnig on port ${PORT}`);
+console.log(`Server Is Running on port ${PORT}`);
 });
 
-app.get("/books", (req, res) => {
+app.get("/books",async (req, res) => {
+const books = await Book.findAll();
   res.json(books);
+
+  //await res.json(Book.findAll());//would this work even?????
 });
 
-app.get("/books/:id", (req,ses) => {
-    const book = books.find((book) => book.id === Number(req.params.id));
+app.get("/books/:id", async (req,res) => {
+    const book = await Book.findByPk(req.params.id);
 
     if(!book){
-        return res.status(404).json({Message: "Book not found"})
+        return res.status(404).json({ 
+            error: "Book not found"
+         });
     }
-    res.json(boo)
+    res.json(book)
 })
+
+// POST create a new book
+app.post('/books', async (req, res) => {
+
+     if (!req.body.title || !req.body.author || !req.body.status) {
+  return res.status(400).json({
+    error: "Missing required fields"
+  });
+}
+const newBook = await Book.create({
+  title: req.body.title,
+  author: req.body.author,
+  status: req.body.status
+});
+
+res.status(201).json(newBook);
+});
+
+// PUT update book
+
+app.put("/books/:id", async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+
+  if (!book) {
+    return res.status(404).json({
+      error: "Book not found"
+    });
+  }
+
+  book.title = req.body.title;
+  book.author = req.body.author;
+  book.status = req.body.status;
+
+  await book.save();
+
+  res.json(book);
+});
+
+// DELETE a book
+app.delete('/books/:id', (req, res) => {
+  const bookIndex = books.findIndex(book => book.id === Number(req.params.id));
+  if (bookIndex === -1) return res.status(404).send('Book not found');
+  const deletedBook = books.splice(bookIndex, 1);
+  res.json(deletedBook[0]);
+});
